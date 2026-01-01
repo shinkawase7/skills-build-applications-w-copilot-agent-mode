631f3fbf7902bd1abb77a19315559961ece01a8c
shinkawase7 <shin.kawase7@gmail.com>
2026-01-01 22:50:13 +0000

    Add initial Django project structure with models, views, serializers, and API endpoints

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,62 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from djongo import models
+
+# Define models if not already defined in models.py (for demonstration)
+class Team(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Activity(models.Model):
+    user = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    duration = models.IntegerField()
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Leaderboard(models.Model):
+    user = models.CharField(max_length=100)
+    score = models.IntegerField()
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Workout(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        # Delete all data
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create teams
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+
+        # Create users
+        ironman = User.objects.create_user(username='ironman', email='ironman@marvel.com', password='password')
+        batman = User.objects.create_user(username='batman', email='batman@dc.com', password='password')
+
+        # Create activities
+        Activity.objects.create(user='ironman', type='run', duration=30)
+        Activity.objects.create(user='batman', type='cycle', duration=45)
+
+        # Create leaderboard
+        Leaderboard.objects.create(user='ironman', score=100)
+        Leaderboard.objects.create(user='batman', score=90)
+
+        # Create workouts
+        Workout.objects.create(name='Pushups', description='Do 20 pushups')
+        Workout.objects.create(name='Situps', description='Do 30 situps')
+
+        self.stdout.write(self.style.SUCCESS('Test data populated successfully.'))
