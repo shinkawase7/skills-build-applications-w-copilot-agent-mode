2ac8bab5d7544af24ca554f55a6ab9c6de2a663e
shinkawase7 <shin.kawase7@gmail.com>
2026-01-01 23:03:06 +0000

    Update settings and URLs to support Codespaces environment

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-jkf%)8lm#ju8v8+#52a+pg3c6@=44)-)07rbfh6iw1%y8gn2*+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f"{CODESPACE_NAME}-8000.app.github.dev")
 
 
 # Application definition
